ab4538c9dae874fbc482d33b79dc8f96897e86f3
HSGill <harjotgill73@gmail.com>
2018-09-23 21:14:34 +1200

    working

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-ss
+var button = document.getElementById("Enter");
+var inputText =document.getElementById("userInput");
+
+var ul =  document.getElementById("num1");
+
+
+button.addEventListener("click", function(){
+	if (inputText.value.length>0){
+	
+	var li= document.createElement("li");
+	li.appendChild(document.createTextNode(inputText.value));
+	
+	ul.appendChild(li);
+	inputText.value ="";
+	}
+      
+})
